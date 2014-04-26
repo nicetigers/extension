19368b8afd9bd8bf9ece7d4c5d3defe0a1b0f46a
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2014-04-26 19:56:16 -0400

    Removed stale code

--- src/background.js
+++ src/background.js
@@ -1,21 +1,12 @@
-/*
-$($('#iframe')[0].contentDocument).on('ready', function(ev){
-    $('#loading').remove();
-   });
-*/
- 
- /* $("#iframe").on('onload', function (ev) {
-   //alert('iFrame loaded!');
-   $('#loading').remove();
-}); */
-
 $(function() {
-      $('<iframe />',{ id: 'frame1',
-                 class:'myframe',
-                 height: "100%",
-                 width: "100%",
-                 src: 'http://newice.herokuapp.com',
-                 frameborder: "0"
+      $('<iframe />',
+      {
+         id: 'frame1',
+         class:'myframe',
+         height: "100%",
+         width: "100%",
+         src: 'http://newice.herokuapp.com',
+         frameborder: "0"
   }).load(function(e){  
     $('#loading').remove();
   }).appendTo($('#container_body'));
